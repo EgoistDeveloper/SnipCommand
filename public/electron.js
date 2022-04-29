a327bb84be9f614d8bcde104f80e119582023164
Egoist <epsilonelittr@gmail.com>
2022-04-29 21:38:30 +0300

    Add remember window size and position feature

--- public/electron.js
+++ public/electron.js
@@ -1,41 +1,58 @@
-const { app, BrowserWindow, ipcMain, session } = require('electron');
-const path = require('path');
-const isDev = require('electron-is-dev');
+const { app, BrowserWindow, ipcMain, session } = require("electron");
+const path = require("path");
+const isDev = require("electron-is-dev");
 const { menu } = require("./menu");
+const config = require("electron-json-config").factory();
 
 let mainWindow;
 const isWindows = process.platform === "win32";
 
-
 app.on("ready", async () => {
-    mainWindow = new BrowserWindow({
-        width: 1366,
-        height: 768,
-        icon: `${__dirname}/images/logo/snip_command.png`,
-        titleBarStyle: "hidden",
-        backgroundColor: '#fff',
-        frame: !isWindows,
-        webPreferences: {
-            nodeIntegration: true,
-            enableRemoteModule: true
-        }
-    });
+  const options = Object.assign(
+    {
+      width: 1366,
+      height: 768,
+      icon: `${__dirname}/images/logo/snip_command.png`,
+      titleBarStyle: "hidden",
+      backgroundColor: "#fff",
+      frame: !isWindows,
+      webPreferences: {
+        nodeIntegration: true,
+        enableRemoteModule: true,
+      },
+    },
+    config.get("winBounds")
+  );
 
-    mainWindow.maximize();
-    mainWindow.loadURL(isDev ? 'http://localhost:3000' : `file://${path.join(__dirname, '../build/index.html')}`).then(r => r);
+  mainWindow = new BrowserWindow(options);
 
-    if (isDev) {
-        mainWindow.webContents.openDevTools();
-    }
-    mainWindow.on('closed', () => mainWindow = null);
+  mainWindow
+    .loadURL(
+      isDev
+        ? "http://localhost:3000"
+        : `file://${path.join(__dirname, "../build/index.html")}`
+    )
+    .then((r) => r);
+
+  if (isDev) {
+    mainWindow.webContents.openDevTools();
+  }
+
+  mainWindow.on("close", (e) => {
+    config.set("winBounds", mainWindow.getBounds());
+  });
+
+  mainWindow.on("closed", (e) => {
+    mainWindow = null;
+  });
 });
 
 app.disableHardwareAcceleration();
 
-app.on('window-all-closed', () => {
-    if (process.platform !== 'darwin') {
-        app.quit();
-    }
+app.on("window-all-closed", () => {
+  if (process.platform !== "darwin") {
+    app.quit();
+  }
 });
 
 // app.on('activate', () => {
@@ -45,5 +62,7 @@ app.on('window-all-closed', () => {
 // });
 
 ipcMain.handle(`display-app-menu`, (e, args) => {
-    if (isWindows && mainWindow) menu.popup({ window: mainWindow, x: args.x, y: args.y });
+  if (isWindows && mainWindow) {
+    menu.popup({ window: mainWindow, x: args.x, y: args.y });
+  }
 });
